84f69223fc2ed800319a57fb0639af15f7fbf4a3
taimoor954 <taimoormuhammad954@gmail.com>
2022-09-10 18:48:21 +0500

    done till creating user

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import { prisma, PrismaClient } from "@prisma/client"
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    console.log('Worked')
+    return res.status(200).json({ name: 'John Doe' })
+
+    // Process a POST request
+  }
+
+  if (req.method === 'POST') {
+    let prisma = new PrismaClient();
+    console.log(prisma.user, 'here is user')
+
+    let data = await prisma.user.create({ data: { createdAt: new Date(), email: "taimoormuhammad951@gmail.com", name: "Taimoor" } })
+    console.log('Worked')
+    return res.status(200).json({ name: data })
+
+    // Process a POST request
+  }
+  else {
+    console.log('NO')
+    return res.status(404).json({ name: 'NOt a valid route' })
+
+  }
+
 }
